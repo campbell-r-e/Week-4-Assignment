591dfc9fd721c84b603759d949f286392cbed9d9
bsucs <122378640+bsucs@users.noreply.github.com>
2024-09-16 16:38:23 -0400

    intial

---
+++ week4/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import React from 'react';
+import home from './Gallery';
+
+
+
+export default function Home() {
+  let arrayName: String[];
+  return (
+    
+    arrayName= []
+  );
+}
