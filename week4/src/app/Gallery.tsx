8895800c7406511ca221b3e3dab5287cf8a35298
bsucs <122378640+bsucs@users.noreply.github.com>
2024-09-17 17:34:30 -0400

    third

--- week4/src/app/Gallery.tsx
+++ week4/src/app/Gallery.tsx
@@ -1,7 +1,49 @@
-import React from 'react'; 
+"use client";
+import React, { useState } from 'react'; 
+interface images{
+  pics: string[];
+}
 
-export default function Gallery() {
+export default function Gallery({ pics }:images) {
+  const [imageIndex, setimageIndex] = useState(0);
+  
+  
+  const image_calc_up = () => {
+    if(imageIndex<pics.length-1){
+     
+      setimageIndex(imageIndex + 1); 
+    }else{
+      setimageIndex(0); 
+    }
+
+    
+   
+  };
+  const image_calc_down = () => {
+    if(imageIndex>0){
+     
+      setimageIndex(imageIndex - 1); 
+    }else{
+      setimageIndex(pics.length-1); 
+    }
+
+    
+   
+  };
+
+
+
   return (
-    <div></div>
+    
+    <div>
+      <title>Ham Radio</title>
+      <h1>Ham radio Pictures</h1>
+      <img src={`/${pics[imageIndex]}`}width="700" height="700"></img>
+      <button onClick= {image_calc_down}>Previous</button>
+      
+      <button onClick= {image_calc_up}>Next</button>
+
+      
+    </div>
   );
 }
--- week4/src/app/page.tsx
+++ week4/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 import React from 'react';
 import Gallery from './Gallery';
 
@@ -6,12 +6,12 @@ import Gallery from './Gallery';
 
 
 export default function Home() {
-  let pictures: String[];
-  pictures = ["th-257426696.jpg","th-2696583940.jpg","th-2921091812.jpg"]
+   
+   const pictures = ["pictures/th-257426696.jpg","pictures/th-2696583940.jpg","pictures/th-2921091812.jpg"]
  
 
   return (
-   <Gallery images={pictures} /> 
+   <Gallery pics={pictures} /> 
     
    
      
